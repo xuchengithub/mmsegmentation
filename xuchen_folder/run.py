54dabd30457e4822977bde97e90286d2552e4834
Your Name <you@example.com>
2023-06-08 12:53:14 +0900

    add_origin_foler

---
+++ xuchen_folder/run.py
@@ -0,0 +1,88 @@
+from mmengine import Config
+baseline_cfg_path = '/mmsegmentation/xuchen_folder/work_dirs/xuchen_mask_segmetation/mask2former_swin-s_8xb2-160k_xuchen-512x512.py'
+# /mmsegmentation/configs/mask2former/mask2former_swin-s_8xb2-160k_xuchen-512x512.py
+cfg = Config.fromfile(baseline_cfg_path)
+model_name = baseline_cfg_path.split("/")[-1].split(".")[0]
+print(f"config base model of :{model_name} ")
+
+import mmcv
+import os
+from mmseg.apis import init_model, inference_model, show_result_pyplot
+# Init the model from the config and the checkpoint
+checkpoint_path = cfg.work_dir +'/best_mIoU_iter_110000.pth'
+model = init_model(cfg, checkpoint_path, 'cuda:0')
+
+# 推理演示图像
+import pylab as plt
+import time
+
+
+data_folder_name ="/mmsegmentation/xuchen_folder/powertain_goods_data"
+
+data_address = data_folder_name +"/test_images"
+how_much_file = os.listdir(data_address)
+filtered_list = list(filter(lambda how_much_file: how_much_file.endswith(".png"), how_much_file))
+# model.cfg=cfg
+print(f"test_image_address:{data_address}")
+print(f"how_much_file:{len(filtered_list)}")
+print(f"image_list:{filtered_list} ")
+
+
+import mmengine
+mmengine.mkdir_or_exist(data_folder_name + "/result/")
+mmengine.mkdir_or_exist(data_folder_name + "/splited_images_result/")
+
+import time
+import matplotlib.pyplot as plt
+
+
+for i in range(len(filtered_list)):
+
+    image_nn = data_folder_name + "/test_images/" + filtered_list[i][0:-4]
+    img_path = image_nn + ".png"
+    print(f"origin_image_path:{img_path}")
+    img = mmcv.imread(img_path)
+    resized_img = mmcv.imrescale(img,(512,512))
+    print(f"img image size is:{resized_img.shape}")
+    # save_resize = image_result_path + "_resize.png"
+    # print(f"save_resize_dir:{save_resize}")
+
+
+        
+    start_time = time.time()
+    result = inference_model(model, resized_img)
+    # print(result)
+
+    
+    # Assuming seg_data is your input data
+    seg_data = result
+    
+    # print(result)
+    # Extracting fields from the data
+    pred_sem_seg_data = seg_data.pred_sem_seg.data.cpu().numpy().squeeze()
+    
+    end_time = time.time()
+    print(f"run time is :{end_time-start_time}")
+
+    image_result_path = data_folder_name + "/result/" + filtered_list[i][0:-4]
+    save_resuit_dir = image_result_path + "_result.png"
+    print(f"save_resuit_dir:{save_resuit_dir}")
+    
+    vis_result = show_result_pyplot(model, resized_img,result, draw_gt=False,show=False,out_file=save_resuit_dir)
+
+
+    # # Displaying the images
+    # plt.figure(figsize=(12, 6))
+    
+    # # Displaying pred_sem_seg image
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(pred_sem_seg_data) 
+    # plt.title('pred_sem_seg')
+    
+    # # Displaying seg_logits image
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(resized_img)
+    # plt.title('seg_logits')
+
+    # plt.show()
+    
---
+++ xuchen_folder/segmenter_vit-s_mask_8xb1-160k_ade20k-512x512.py
@@ -0,0 +1,204 @@
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segmenter/vit_small_p16_384_20220308-410f6037.pth'
+backbone_norm_cfg = dict(type='LN', eps=1e-06, requires_grad=True)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[127.5, 127.5, 127.5],
+    std=[127.5, 127.5, 127.5],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255,
+    size=(512, 512))
+model = dict(
+    type='EncoderDecoder',
+    data_preprocessor=dict(
+        type='SegDataPreProcessor',
+        mean=[127.5, 127.5, 127.5],
+        std=[127.5, 127.5, 127.5],
+        bgr_to_rgb=True,
+        pad_val=0,
+        seg_pad_val=255,
+        size=(512, 512)),
+    pretrained=
+    'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segmenter/vit_small_p16_384_20220308-410f6037.pth',
+    backbone=dict(
+        type='VisionTransformer',
+        img_size=(512, 512),
+        patch_size=16,
+        in_channels=3,
+        embed_dims=384,
+        num_layers=12,
+        num_heads=6,
+        drop_path_rate=0.1,
+        attn_drop_rate=0.0,
+        drop_rate=0.0,
+        final_norm=True,
+        norm_cfg=dict(type='LN', eps=1e-06, requires_grad=True),
+        with_cls_token=True,
+        interpolate_mode='bicubic'),
+    decode_head=dict(
+        type='SegmenterMaskTransformerHead',
+        in_channels=384,
+        channels=384,
+        num_classes=150,
+        num_layers=2,
+        num_heads=6,
+        embed_dims=384,
+        dropout_ratio=0.0,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(480, 480)))
+dataset_type = 'XuchenDataset'
+data_root = 'powertain_goods_data/'
+crop_size = (512, 512)
+image_scale = (256, 256)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='RandomResize',
+        scale=(256, 256),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(128, 128), cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackSegInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(256, 256), keep_ratio=True),
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+img_ratios = [0.5, 0.75, 1.0, 1.25]
+tta_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(
+        type='TestTimeAug',
+        transforms=[[{
+            'type': 'Resize',
+            'scale_factor': 0.5,
+            'keep_ratio': True
+        }, {
+            'type': 'Resize',
+            'scale_factor': 0.75,
+            'keep_ratio': True
+        }, {
+            'type': 'Resize',
+            'scale_factor': 1.0,
+            'keep_ratio': True
+        }, {
+            'type': 'Resize',
+            'scale_factor': 1.25,
+            'keep_ratio': True
+        }],
+                    [{
+                        'type': 'RandomFlip',
+                        'prob': 0.0,
+                        'direction': 'horizontal'
+                    }, {
+                        'type': 'RandomFlip',
+                        'prob': 1.0,
+                        'direction': 'horizontal'
+                    }], [{
+                        'type': 'LoadAnnotations'
+                    }], [{
+                        'type': 'PackSegInputs'
+                    }]])
+]
+train_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='InfiniteSampler', shuffle=True),
+    dataset=dict(
+        type='XuchenDataset',
+        data_root='powertain_goods_data/',
+        data_prefix=dict(
+            img_path='leftImg8bit/train', seg_map_path='gtFine/train'),
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations'),
+            dict(
+                type='RandomResize',
+                scale=(256, 256),
+                ratio_range=(0.5, 2.0),
+                keep_ratio=True),
+            dict(type='RandomCrop', crop_size=(128, 128), cat_max_ratio=0.75),
+            dict(type='RandomFlip', prob=0.5),
+            dict(type='PhotoMetricDistortion'),
+            dict(type='PackSegInputs')
+        ]))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='XuchenDataset',
+        data_root='powertain_goods_data/',
+        data_prefix=dict(
+            img_path='leftImg8bit/val', seg_map_path='gtFine/val'),
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='Resize', scale=(256, 256), keep_ratio=True),
+            dict(type='LoadAnnotations'),
+            dict(type='PackSegInputs')
+        ]))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='XuchenDataset',
+        data_root='powertain_goods_data/',
+        data_prefix=dict(
+            img_path='leftImg8bit/val', seg_map_path='gtFine/val'),
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='Resize', scale=(256, 256), keep_ratio=True),
+            dict(type='LoadAnnotations'),
+            dict(type='PackSegInputs')
+        ]))
+val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
+test_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
+default_scope = 'mmseg'
+env_cfg = dict(
+    cudnn_benchmark=True,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'))
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    type='SegLocalVisualizer',
+    vis_backends=[dict(type='LocalVisBackend')],
+    name='visualizer')
+log_processor = dict(by_epoch=False)
+log_level = 'INFO'
+load_from = None
+resume = False
+tta_model = dict(type='SegTTAModel')
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0),
+    clip_grad=None)
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=0.0001,
+        power=0.9,
+        begin=0,
+        end=20000,
+        by_epoch=False)
+]
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=20000, val_interval=2000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
